0874d549556dc9d0916cf3fe2f30b86b3fec73cf
Osayuki Okhuarobo <osayukiraymond@gmail.com>
2024-06-28 00:01:56 +0100

    added toggle click event functionality to mobile menu icon

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,9 @@
-ECHO is on.
+"strict";
+
+// global variables
+const menuIcon = document.querySelector("#hamburger_menu");
+
+// Hamburger Menu
+menuIcon.addEventListener("click", function () {
+  this.classList.toggle("open");
+});
