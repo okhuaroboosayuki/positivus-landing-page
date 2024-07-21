51b60ac74617dd5a00c3d1304c46e51c7036a945
Osayuki Okhuarobo <osayukiraymond@gmail.com>
2024-07-21 08:07:45 +0100

    feat: add menu_open when menuIcon is toggled

--- JS/index.js
+++ JS/index.js
@@ -2,8 +2,15 @@
 
 // global variables
 const menuIcon = document.querySelector("#hamburger_menu");
+const navListElement = document.querySelector("#list");
 
 // Hamburger Menu
 menuIcon.addEventListener("click", function () {
   this.classList.toggle("open");
+
+  if (menuIcon.classList.contains("open")) {
+    navListElement.classList.add("menu_open");
+  } else {
+    navListElement.classList.remove("menu_open");
+  }
 });
