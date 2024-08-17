5da0c643e7fee7546609991f6ac7558468c7bcb3
okhuaroboosayuki <osayukiraymond@gmail.com>
2024-08-17 10:33:03 +0100

    feat: added scroll display animation using IntersectionObserver

--- JS/index.js
+++ JS/index.js
@@ -3,6 +3,7 @@
 // global variables
 const menuIcon = document.querySelector("#hamburger_menu");
 const navListElement = document.querySelector("#list");
+const hiddenEl = document.querySelectorAll(".hidden");
 
 // Hamburger Menu
 menuIcon.addEventListener("click", function () {
@@ -14,3 +15,15 @@ menuIcon.addEventListener("click", function () {
     navListElement.classList.remove("menu_open");
   }
 });
+
+// scroll animate
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+
+    if (entry.isIntersecting) entry.target.classList.add("show");
+    else entry.target.classList.remove("show");
+  });
+});
+
+hiddenEl.forEach((el) => observer.observe(el));
