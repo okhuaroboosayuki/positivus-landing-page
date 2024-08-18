cedec00ef2cc58adb0c74a2454e05cdcef76ea09
okhuaroboosayuki <osayukiraymond@gmail.com>
2024-08-18 23:42:55 +0100

    feat: created an animated scroll using a scroll event listener

--- JS/index.js
+++ JS/index.js
@@ -3,6 +3,8 @@
 // global variables
 const menuIcon = document.querySelector("#hamburger_menu");
 const navListElement = document.querySelector("#list");
+const clientRow = document.querySelector(".client_row");
+
 const hiddenEl = document.querySelectorAll(".hidden");
 
 // Hamburger Menu
@@ -16,14 +18,22 @@ menuIcon.addEventListener("click", function () {
   }
 });
 
-// scroll animate
+// scroll handling
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
-
     if (entry.isIntersecting) entry.target.classList.add("show");
     else entry.target.classList.remove("show");
   });
 });
 
 hiddenEl.forEach((el) => observer.observe(el));
+
+window.addEventListener("scroll", function () {
+  const scrollPosition = this.scrollY;
+
+  const speed = 0.3;
+
+  const translateValue = scrollPosition * speed;
+
+  clientRow.style.transform = `translateX(-${translateValue}px)`;
+});
