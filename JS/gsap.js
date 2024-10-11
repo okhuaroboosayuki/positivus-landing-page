eb3c60c1294ac38114343712663b46a8b5ab1da8
okhuaroboosayuki <osayukiraymond@gmail.com>
2024-10-11 20:59:17 +0100

    feat: added animation for work process section; fix: fixed minor issues related to tl function expression

--- JS/gsap.js
+++ JS/gsap.js
@@ -15,15 +15,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const clientRow = section.querySelector(".client_row");
     const serviceCards = section.querySelectorAll(".card");
     const serviceCardsContainer = section.querySelectorAll(".cards_container");
+    const ctaIllustration = section.querySelector(".c_t_a-right");
+    const caseStudiesCardContainer = section.querySelectorAll(".case-studies_bottom-card_container");
+    const workProcessBottom = section.querySelector(".work_process_bottom");
+    const workProcessBottomBox = section.querySelectorAll(".work_process_bottom_box");
 
-    function tl(trigger, start = "top 10%", end = "bottom 10%") {
+    const toggleActionsOption = "play none none none";
+    function tl(trigger, { start = "top 10%", end = "bottom 10%", toggleActions = "play none none reverse" } = {}) {
       return gsap.timeline({
         scrollTrigger: {
           trigger: trigger,
           start: start,
           end: end,
-          toggleActions: "play none none reverse",
-          markers: true,
+          toggleActions: toggleActions,
         },
       });
     }
@@ -32,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // if the current section is the hero section, & the screenWidth is > 900, apply the animation based on the current section; else, apply based on each of the current section's child elements.
     if (heading === section.querySelector(".hero_left h3") && subTitle === section.querySelector(".hero_left p")) {
       if (isLargeScreen) {
-        const heroTl = tl(section, "15%", "+=300");
+        const heroTl = tl(section, { start: "15%", end: "+=300" });
         heroTl
           .to(heading, { opacity: 0, translateY: "-100px", ease: "expo.in", duration: 0.5 })
           .to(subTitle, { opacity: 0, translateX: "-100px", ease: "expo.in" }, "<")
@@ -53,14 +57,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Client Row Animations
     if (clientRow && isSmallScreen) {
-      tl(section, "top top", "+=300").to(clientRow, { opacity: 0, scale: 0.1, ease: "power1.inOut" });
+      tl(section, { start: "top top", end: "+=300" }).to(clientRow, { opacity: 0, scale: 0.1, ease: "power1.inOut" });
     }
 
     // Service Cards Animations:
     // if the screen width is less than 500, it loops through each service card to apply the animation; else it loops through each service card container to apply the animation
     if (serviceCardsContainer.length > 0) {
       serviceCards.forEach((card) => {
-        tl(card, "top top", "+=300").to(card, {
+        tl(card, { start: "top top", end: "+=300" }).to(card, {
           opacity: 0,
           translateX: () => (Math.random() > 0.5 ? "100px" : "-100px"),
           ease: "expo.in",
@@ -82,7 +86,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // CTA Section Animations
-    const ctaIllustration = section.querySelector(".c_t_a-right");
     if (ctaIllustration) {
       tl(ctaIllustration, "10%").to(ctaIllustration, {
         opacity: 0,
@@ -93,19 +96,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Case Studies Section Animations
-    const caseStudiesSection = section.querySelector("#case-studies");
-    const caseStudiesBottom = section.querySelector(".case_studies_bottom");
-    const caseStudiesCardContainer = section.querySelectorAll(".case-studies_bottom-card_container");
-
-    if (caseStudiesSection && isLargeScreen) {
-      console.log(caseStudiesSection);
-
-      tl(caseStudiesSection, "top 75%").from(caseStudiesBottom, { opacity: 0, translateY: "100Px", ease: "expo.in" });
-    }
 
     caseStudiesCardContainer.forEach((container) => {
       if (isLargeScreen) {
-        tl(container.parentElement).to(caseStudiesCardContainer, {
+        tl(container.parentElement, { start: "20%" }).to(caseStudiesCardContainer, {
           opacity: 0,
           translateY: "100px",
           ease: "expo.in",
@@ -115,12 +109,32 @@ document.addEventListener("DOMContentLoaded", function () {
       } else {
         tl(container).to(container, {
           opacity: 0,
-          translateX: "100px",
+          translateX: () => (Math.random() > 0.5 ? "100px" : "-100px"),
           ease: "expo.in",
           stagger: 0.2,
           delay: 0.5,
         });
       }
     });
+
+    // Work Process Section Animations
+
+    if (workProcessBottom) {
+      workProcessBottomBox.forEach((box) => {
+        tl(workProcessBottom, { start: "-30%", toggleActions: "play none none none" }).from(box, 1, {
+          opacity: 0,
+          y: 100,
+          stagger: {
+            each: 0.1,
+          },
+        });
+
+        tl(box, { start: "-20%" }).to(box, {
+          opacity: 0,
+          translateX: () => (Math.random() > 0.5 ? "100px" : "-100px"),
+          ease: "sine.out",
+        });
+      });
+    }
   });
 });
